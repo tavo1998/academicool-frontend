8d35153c19008d3a3a278881b8ce0b6b36b13845
Tavo <gustavo.pinto@correounivalle.edu.co>
2022-03-23 18:38:04 -0500

    init config

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Testing...</div>
+    </div>
+  )
+}
+
+export default Home
