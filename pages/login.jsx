151720dc5ca652baa183ae29332e17c34e5d9c2f
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-03-24 14:23:18 -0500

    created login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,16 +1,26 @@
+import LoginTitle from "../components/Login/LoginTitle"
+import LoginLema from "../components/Login/LoginLema";
+import GoogleButton from "../components/Login/GoogleButton";
+import RecoveryInformation from "../components/Login/RecoveryInformation";
+import CopyrightFooter from "../components/Login/CopyrightFooter";
+
 const LoginPage = () => {
   return (
-    <div className="flex flex-col justify-center h-screen px-4 space-y-5">
-      <h1 className="text-5xl text-primaryColor font-bold text-center">
-        Bienvenidos a <span className="underline underline-offset-2">Academicool</span>
-      </h1>
-      <p className="text-primaryColor text-md text-center">
-        Obten información en tiempo real sobre la situación académica de tu estudiante
-      </p>
-      <button className="w-full flex justify-center bg-primaryColor text-white text-md py-1 rounded-sm font-bold">Ingresar con Google</button>
-      <div className="space-y-1">
-        <p className="text-sm text-primaryColor text-center">¿Tienes problemas para iniciar sesión?</p>
-        <p className="text-sm text-primaryColor text-center underline">Envía un mensaje a soporte</p>
+    <div className="grid grid-cols-1 lg:grid-cols-2 h-screen">
+      <div className="hidden lg:block bg-primaryColor z-10" />
+      <div className="relative flex flex-col justify-between px-4">
+        <div className="absolute lg:w-32 w-48 lg:h-32 h-48 bg-accentColor top- rounded-full -left-4 -top-4" />
+        <div className="absolute lg:w-28 w-40 lg:h-28 h-40 bg-primaryColor top- rounded-full top-14 left-20 z-10" />
+        <div className="absolute lg:w-20 w-32 lg:h-20 h-32 bg-secondaryColor top- rounded-full top-10 lg:left-40 left-48" />
+        <div className="flex flex-col justify-center flex-1 space-y-5 mt-16">
+          <LoginTitle/>
+          <LoginLema />
+          <GoogleButton />
+          <RecoveryInformation />
+        </div>
+        <footer className="flex justify-center mb-4">
+          <CopyrightFooter />
+        </footer>
       </div>
     </div>
   )
