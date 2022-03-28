859b9f1ad86b808a0143bd290559b611979a01cb
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-03-28 18:47:17 -0500

    created section header component

---
+++ components/side_menu/SectionHeader.jsx
@@ -0,0 +1,13 @@
+const SectionHeader = ({ text, icon: Icon }) => {
+  return (
+    <div>
+      <div className="flex items-center overflow-hidden">
+      { Icon && <Icon className="text-white mr-2"/> }
+      <h1 className="text-white font-semibold">{text}</h1>
+      </div>
+      <div className="w-full h-[1px] bg-white mt-1"/>
+    </div>
+  )
+}
+
+export default SectionHeader;
