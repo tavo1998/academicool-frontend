cbf0378cc5b37dbc1a454ddbe5770c7d951da6d6
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-03-30 19:24:37 -0500

    modified institution edit component style

--- components/admin_dashboard/InstitutionDetailSection.jsx
+++ components/admin_dashboard/InstitutionDetailSection.jsx
@@ -5,80 +5,46 @@ import TextInputField from "../common/TextInputField"
 
 const InstitutionDetailSection = () => {
   return (
-    <>
+    <div className="lg:w-1/2 lg:mx-auto">
       <OptionHeader title="Editar Institución"/>
       <div className="mb-2"/>
-      <div className="lg:hidden">
+      <div className="grid grid-cols-1 lg:grid-cols-2 space-y-1 lg:space-y-0 gap-4">
         <TextInputField 
           title="Nombre"
           placeholder="Nombre de la Institución"
         />
-        <div className="mb-2"/>
-        <div className="flex space-x-2">
-          <TextInputField 
-            title="Ciudad"
-            placeholder="Nombre de la ciudad"
-          />
+        <TextInputField 
+          title="Ciudad"
+          placeholder="Nombre de la ciudad"
+        />
+        <div className="lg:col-span-2">
           <TextInputField 
             title="Dirección"
             placeholder="Dirección"
           />
         </div>
-        <div className="mb-2"/>
-        <TextAreaField 
-          title="Misión"
-          rows={6}
-          maxLength={255}
-          placeholder="Escriba una misión"
-        />
-        <TextAreaField 
-          title="Visión"
-          rows={6}
-          maxLength={255}
-          placeholder="Escriba una visión"
-        />
-        <div className="flex space-x-2 mt-2">
-          <AccentButton text="Cancelar"/>
-          <AccentButton text="Guardar Cambios"/>
-        </div>
-      </div>
-      <div className="hidden lg:block">
-        <div className="flex justify-between space-x-8">
-          <TextInputField 
-            title="Nombre"
-            placeholder="Nombre de la Institución"
-          />
-          <TextInputField 
-              title="Ciudad"
-              placeholder="Nombre de la ciudad"
-            />
-          <TextInputField 
-            title="Dirección"
-            placeholder="Dirección"
-          />
-        </div>
-        <div className="flex space-x-8 mt-4">
+        <div className="lg:col-span-2">
           <TextAreaField 
             title="Misión"
-            rows={5}
+            rows={6}
             maxLength={255}
             placeholder="Escriba una misión"
           />
+        </div>
+        <div className="lg:col-span-2">
           <TextAreaField 
             title="Visión"
-            rows={5}
+            rows={6}
             maxLength={255}
             placeholder="Escriba una visión"
           />
         </div>
-        <div className="flex justify-center mt-6">
-          <div className="w-1/2 flex space-x-8">
-            <AccentButton text="Cancelar"/>
-            <AccentButton text="Guardar Cambios"/>
-          </div>
-        </div>
       </div>
-    </>
+      <div className="flex space-x-2 mt-2 lg:mt-4">
+        <AccentButton text="Cancelar"/>
+        <AccentButton text="Guardar Cambios"/>
+      </div>
+    </div>
   )
 }
 
--- components/common/TextInputField.jsx
+++ components/common/TextInputField.jsx
@@ -1,6 +1,6 @@
 const TextInputField = ({ title, placeholder }) => {
   return (
-    <label className="block w-full">
+    <label className="w-full">
       <h1 className="text-customGrey font-semibold text-sm lg:text-lg">{title}</h1>
       <input 
         className="bg-customGrey bg-opacity-10 p-2 w-full lg:placeholder:text-base placeholder:text-sm mt-1 focus:outline-none text-customGrey text-xs lg:text-base" 
