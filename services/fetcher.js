fc03bb8e11040f0ce85f78f620b0b9ea3996e417
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-01 13:45:02 -0500

    added functions to get, update and create resources

--- services/fetcher.js
+++ services/fetcher.js
@@ -1,10 +1,15 @@
+import RequestError from "../lib/requestError"
+
 const fetcher = async url => {
   try {
     const res = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + url, { credentials: 'include' })
     if(!res.ok) {
-      const error = new Error('An error occurred while fetching the data.')
-      error.info = await res.json()
-      error.status = res.status
+      const error = new RequestError(
+        'An error occurred while fetching the data.',
+        res.status,
+        await res.json()
+      )
+      
       throw error
     }
     return res.json()
