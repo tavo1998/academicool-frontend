212e41699dd048f7b212dd3cff8939a25718ef81
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-01 19:55:40 -0500

    added devtools to zustand store

--- store/index.js
+++ store/index.js
@@ -1,12 +1,13 @@
 import create from 'zustand'
+import { devtools } from 'zustand/middleware'
 
-const useStore = create(set => ({
+const useStore = create(devtools(set => ({
   isSideBarOpen: true,
   itemSideBarSelected: null,
   sectionSelected: { id: null, data: null },
   setIsSideBarOpen: () => set(state => ({ isSideBarOpen: !state.isSideBarOpen })),
   setSectionSelected: (id, data = null) => set(state => ({ sectionSelected: { id, data } })),
   setItemSideBarSelected: (id) => set(state => ({ itemSideBarSelected: id }))
-}))
+})))
 
 export default useStore
