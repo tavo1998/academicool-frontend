f8409ef5d268a74fafc8f1b24dc763c57c7bdaaa
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-02 13:44:31 -0500

    added new state property to handle tab selected

--- store/index.js
+++ store/index.js
@@ -2,12 +2,14 @@ import create from 'zustand'
 import { devtools } from 'zustand/middleware'
 
 const useStore = create(devtools(set => ({
-  isSideBarOpen: true,
+  isSideBarOpen: false,
   itemSideBarSelected: null,
   sectionSelected: { id: null, data: null },
+  tabSelected: null,
   setIsSideBarOpen: () => set(state => ({ isSideBarOpen: !state.isSideBarOpen })),
   setSectionSelected: (id, data = null) => set(state => ({ sectionSelected: { id, data } })),
-  setItemSideBarSelected: (id) => set(state => ({ itemSideBarSelected: id }))
+  setItemSideBarSelected: (id) => set(state => ({ itemSideBarSelected: id })),
+  setTabSelected: (id) => set(state => ({ tabSelected: id }))
 })))
 
 export default useStore
