053a0603ccab9a4a3be786fdf6bd0f054bfca70d
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-03 10:39:08 -0500

    created assistance checkbox

--- components/teacher_dashboard/AssistanceTab.jsx
+++ components/teacher_dashboard/AssistanceTab.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
 import Calendar from "../common/Calendar"
+import AssistanceCheckBox from "./AssitanceCheckBox"
 
 const AssistanceTab = () => {
+  const [selectedDate, setSelectedDate] = useState(null)
   const handleDate = (date) => {
-    console.log(date.toString())
+    setSelectedDate(date)
   }
 
   return (
     <div>
       <Calendar handleDate={handleDate} />
+      { selectedDate ? <h1 className="mt-2">{selectedDate.toString()}</h1>  : <></>}
+      <AssistanceCheckBox
+        className="mt-2"
+      />
+      <AssistanceCheckBox
+        className="mt-2"
+      />
+      <AssistanceCheckBox
+        className="mt-2"
+      />
+      
     </div>
   )
 }
