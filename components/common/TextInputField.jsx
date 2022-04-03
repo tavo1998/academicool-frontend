7dfcaf924e511be3028154975e4923f60c90614a
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-03 12:00:05 -0500

    changed text input field bg color

--- components/common/TextInputField.jsx
+++ components/common/TextInputField.jsx
@@ -5,7 +5,7 @@ const TextInputField = React.forwardRef(({ title, placeholder, error, ...rest },
     <label className="w-full">
       <h1 className="text-customGrey font-semibold text-sm lg:text-lg">{title}</h1>
       <input 
-        className="bg-customGrey bg-opacity-10 p-2 w-full lg:placeholder:text-base placeholder:text-sm mt-1 focus:outline-none text-customGrey text-xs lg:text-base" 
+        className="bg-primaryColor bg-opacity-10 p-2 w-full lg:placeholder:text-base placeholder:text-sm mt-1 focus:outline-none text-customGrey text-xs lg:text-base" 
         type="text" 
         placeholder={placeholder}
         ref={ref}
