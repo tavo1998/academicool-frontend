0e5327a28883e6a5754309601724b10f0340525f
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-04 21:34:58 -0500

    added logic for fetch subject assignments

--- components/teacher_dashboard/AssigmentItem.jsx
+++ components/teacher_dashboard/AssigmentItem.jsx
@@ -1,5 +1,6 @@
 import { useState  } from 'react';
 import { MdModeEdit, MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md"
+import { formatDateString } from '../../lib/calendar';
 
 const AssigmentItem = ({ className, assignment }) => {
   const [itemExpanded, setItemExpanded] = useState(false)
@@ -12,7 +13,7 @@ const AssigmentItem = ({ className, assignment }) => {
     <div className={`${className} bg-primaryColor bg-opacity-10 p-2 rounded-sm`}>
       <div className="flex text-customGrey justify-between items-center">
         <h1 className="text-customGrey text-sm font-semibold">
-          Ejercicio Teorema de Pitagoras
+          {assignment.title}
         </h1>
         <div className="flex items-center space-x-2">
           <MdModeEdit className="text-customGrey" />
@@ -28,12 +29,12 @@ const AssigmentItem = ({ className, assignment }) => {
           }
         </div>
       </div>
-      <span className="text-customGrey text-xs font-normal">2022-03-12</span>
-      <h1 className="text-customGrey text-sm">Fecha de entrega: 2022-03-15</h1>
+      <span className="text-customGrey text-xs font-normal">{formatDateString(assignment.created_at)}</span>
+      <h1 className="text-customGrey text-sm">Fecha de entrega: {formatDateString(assignment.delivery_date)}</h1>
       <h1 className="text-customGrey text-sm">Calificación: No calificado</h1>
       <p 
         className={`${itemExpanded ? 'h-full mt-2' : 'h-0 overflow-hidden'} text-sm text-customGrey`}>
-        Aqui va toda la información respecto a la descripción de la asignación que se ha propuesto
+        { assignment.description }
       </p>
     </div>
   )
--- components/teacher_dashboard/AssignmentTab.jsx
+++ components/teacher_dashboard/AssignmentTab.jsx
@@ -3,9 +3,16 @@ import AccentButton from './../common/AccentButton';
 import AssigmentItem from './AssigmentItem';
 import useStore from './../../store/index'
 import { CREATE_ASSIGNMENT } from '../../config/common';
+import useSWR from 'swr';
+import fetcher from '../../services/fetcher';
 
 const AssignmetTab = () => {
+  const subject = useStore(state => state.sectionSelected.data)
   const setTabSelected = useStore(state => state.setTabSelected)
+  const { data, error } = useSWR(`/api/v1/subjects/${subject.id}/assignments`, fetcher)
+
+  if(error) return <h1>Ocurrió un error</h1>
+  if(!data) return <h1>Cargando</h1>
 
   return (
     <div>
@@ -14,11 +21,9 @@ const AssignmetTab = () => {
         onClick={() => setTabSelected(CREATE_ASSIGNMENT)}
         className="py-2 mt-2 mb-1" 
         text="Crear Asignación" />
-      <AssigmentItem className="mt-2" />
-      <AssigmentItem className="mt-2" />
-      <AssigmentItem className="mt-2" />
-      <AssigmentItem className="mt-2" />
-      <AssigmentItem className="mt-2" />
+      {
+        data.data.map((assignment) => <AssigmentItem className="mt-2" assignment={assignment}/>)
+      }
     </div>
   )
 }
