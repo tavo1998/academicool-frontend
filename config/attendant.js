2fe9c39d15740ee56fd8eab6e110f7d6ae53b3c1
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-09 18:12:40 -0500

    added funcionality for get student subjects

---
+++ config/attendant.js
@@ -0,0 +1,50 @@
+import { SUBJECT_TEACHER_OPTION, PROFILE_TEACHER_OPTION, SIGN_OUT_OPTION } from "./teacher"
+
+export const ASSIGNMENTS_STATISTICS = 'ASSIGNMENTS_STATISTICS'
+export const ASSISTANCES_STATISTICS = 'ASSISTANCES_STATISTICS'
+
+export const createAttendantSideBarOptions = (subjects) => {
+  const attendantOptions = [
+    {
+      name: 'Asignaturas',
+      options: subjects.map(subject => ({
+        id: subject.id,
+        name: subject.name,
+        sectionType: SUBJECT_TEACHER_OPTION,
+        data: subject
+      }))
+    },
+    {
+      name: 'Estadísticas',
+      options: [
+        {
+          id: ASSIGNMENTS_STATISTICS,
+          name: "Calificaciones",
+          sectionType: ASSIGNMENTS_STATISTICS
+        },
+        {
+          id: ASSISTANCES_STATISTICS,
+          name: "Asistencias",
+          sectionType: ASSISTANCES_STATISTICS
+        }
+      ]
+    },
+    {
+      name: 'Configuración',
+      options: [
+        {
+          id: PROFILE_TEACHER_OPTION,
+          name: "Cuenta",
+          sectionType: PROFILE_TEACHER_OPTION
+        },
+        {
+          id: SIGN_OUT_OPTION,
+          name: "Cerrar Sesión",
+          sectionType: SIGN_OUT_OPTION
+        }
+      ]
+    }
+  ]
+
+  return attendantOptions
+}
--- pages/dashboard/attendant.jsx
+++ pages/dashboard/attendant.jsx
@@ -1,21 +1,53 @@
+import { useEffect } from "react"
+import { SIGN_OUT_OPTION } from "../../config/common"
 import { getUserAuthenticated } from "../../services/user"
 import { getRoleRedirectUrl } from "../../lib/redirect"
 import { ATTENDANT_ROLE } from "../../config/common"
+import { createAttendantSideBarOptions } from "../../config/attendant"
 import SelectStudent from "../../components/attendant_dashboard/SelectStudent"
+import NoSectionSelected from "../../components/common/NoSectionSelected"
+import ErrorComponent from "../../components/common/ErrorComponent"
 import useStore from "../../store"
+import useSWR from "swr"
+import SideBar from "../../components/side_menu/SideBar"
+import fetcher from "../../services/fetcher"
 
 const AttendantDashboard = () => {
+  const sectionSelected = useStore(state => state.sectionSelected)
   const studentSelected = useStore(state => state.studentSelected)
+  const { data, error } = useSWR(studentSelected ? `/api/v1/students/${studentSelected.id}/subjects` : null, fetcher)
+
+  useEffect(() => {
+    if(sectionSelected.id === SIGN_OUT_OPTION) router.push(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1/auth/google/sign-out`)
+  }, [sectionSelected.id])
 
   if(!studentSelected){
     return (
       <SelectStudent />
     )
   }
+
+  if(error) {
+    return (
+      <ErrorComponent error={error}>
+        <h1>Ocurrió un error al intentar traer los datos</h1>
+      </ErrorComponent>
+    )  
+  }
+
+  if(!data) return <></>
+
+  const renderSection = () => {
+    switch(sectionSelected.id){
+      case null:
+        return <NoSectionSelected />
+    }
+  }
   
   return (
-    <div>
-      Probando
+    <div className="lg:flex">
+      <SideBar sections={createAttendantSideBarOptions(data.data)} />
+      {renderSection()}
     </div>
   )
 }
