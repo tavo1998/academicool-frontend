e358fe07124b072e4eed479b5d28c9a15d9776d8
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-10 19:01:43 -0500

    added init tab

--- store/index.js
+++ store/index.js
@@ -1,11 +1,12 @@
 import create from 'zustand'
 import { devtools } from 'zustand/middleware'
+import { ASSIGNMENT_TAB } from '../config/common'
 
 const useStore = create(devtools(set => ({
   isSideBarOpen: true,
   itemSideBarSelected: null,
   sectionSelected: { id: null, data: null },
-  tabSelected: null,
+  tabSelected: ASSIGNMENT_TAB,
   tabSelectedData: null,
   studentSelected: null,
   setIsSideBarOpen: () => set(state => ({ isSideBarOpen: !state.isSideBarOpen })),
