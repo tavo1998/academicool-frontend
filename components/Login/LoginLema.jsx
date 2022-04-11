d2a7ab1654eda4089e152502216d3a4b7c57dcdd
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-11 18:51:06 -0500

    changed lema info

--- components/Login/LoginLema.jsx
+++ components/Login/LoginLema.jsx
@@ -1,7 +1,7 @@
 const LoginLema = () => {
   return (
     <p className="text-primaryColor text-center text-md lg:text-2xl">
-      Obten información en tiempo real sobre la situación académica de tu estudiante
+      Modelo integral de seguimiento académico
     </p>
   )
 }
