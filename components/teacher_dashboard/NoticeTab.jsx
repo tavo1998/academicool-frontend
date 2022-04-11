3246463d00b047c280aa415a2dfcc85c3157f04e
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-11 14:02:33 -0500

    added logic for delete subject notices

--- components/teacher_dashboard/NoticeTab.jsx
+++ components/teacher_dashboard/NoticeTab.jsx
@@ -1,22 +1,49 @@
 import { CREATE_NOTICE } from "../../config/common"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { PAGINATION_QUANTITY } from "../../config/common"
+import useSWR, { useSWRConfig } from "swr"
 import AccentButton from "../common/AccentButton"
 import SearchInput from "../common/SearchInput"
 import NoticeItem from "./NoticeItem"
 import ErrorComponent from "../common/ErrorComponent"
 import useStore from "../../store"
-import useSWR from "swr"
 import fetcher from "../../services/fetcher"
+import useMutation from "../../hooks/useMutation"
+import deleter from "../../services/deleter"
 import PaginationButtons from "../common/PaginationButtons"
+import DeleteWarning from "../common/DeleteWarning"
+import DeleteLoading from "../common/DeleteLoading"
+import DeleteError from "../common/DeleteError"
 
+const createSubjectNoticesUrl = (subjectId, search, pagination) => {
+  return `/api/v1/subjects/${subjectId}/notices?title=${search}&pagination=${pagination}`
+}
 
 const NoticeTab = () => {
+  const [showDeleteError, setShowDeleteError] = useState(false)
+  const [noticeToRemove, setNoticeToRemove] = useState(null)
   const [pagination, setPagination] = useState(0)
   const [search, setSearch] = useState('')
   const subject = useStore(state => state.sectionSelected.data)
   const setTabSelected = useStore(state => state.setTabSelected)
-  const { data, error } = useSWR(`/api/v1/subjects/${subject.id}/notices?title=${search}&pagination=${pagination}`, fetcher)
+  const { mutate } = useSWRConfig()
+  const { data, error } = useSWR(createSubjectNoticesUrl(subject.id, search, pagination), fetcher)
+  const { 
+    isSubmitting, 
+    requestError, 
+    requestOk, 
+    sendMutation 
+  } = useMutation(noticeToRemove && `/api/v1/notices/${noticeToRemove.id}`, deleter)
+
+  useEffect(() => {
+    if(requestOk) {
+      mutate(createSubjectNoticesUrl(subject.id, search, pagination))
+    }
+  }, [requestOk])
+
+  useEffect(() => {
+    if(requestError) setShowDeleteError(true)
+  }, [requestError])
 
   const handlePrevPagination = () => {
     setPagination(pagination - 1)
@@ -31,6 +58,23 @@ const NoticeTab = () => {
     setSearch(e.target.value) 
   }
 
+  const handleDeleteNotice = (notice) => {
+    setNoticeToRemove(notice)
+  }
+
+  const handleCancelWarning = () => {
+    setNoticeToRemove(null)
+  }
+
+  const handleAcceptWarning = () => {
+    sendMutation()
+    setNoticeToRemove(null)
+  }
+
+  const handleDeleteError = () => {
+    setShowDeleteError(false)
+  }
+
   const render = () => {
     if(error) {
       return (
@@ -41,7 +85,14 @@ const NoticeTab = () => {
     }
     if(!data) return <h1 className="text-customGrey text-center mt-4">Cargando comunicados...</h1>
     if(data.data.length === 0) return <h1 className="text-customGrey text-center mt-4">No hay comunicados para mostrar</h1>
-    return data.data.map((notice => <NoticeItem key={notice.id} className="mt-2 lg:mt-4" notice={notice}/>))
+    return data.data.map((notice => (
+      <NoticeItem 
+        key={notice.id} 
+        className="mt-2 lg:mt-4" 
+        notice={notice}
+        handleDelete={handleDeleteNotice}
+      />
+    )))
   }
 
   return (
@@ -67,7 +118,26 @@ const NoticeTab = () => {
             handlePrev={handlePrevPagination}
             handleNext={handleNextPagination}
           />
-      ) }
+      )}
+      { noticeToRemove && (
+        <DeleteWarning
+          handleCancel={handleCancelWarning}
+          handleAccept={handleAcceptWarning}
+          info={`Comunicado: ${noticeToRemove.title}`}
+        />
+      )}
+      { isSubmitting && (
+        <DeleteLoading
+          itemType="comunicado"
+        />
+      )}
+      { showDeleteError && (
+        <ErrorComponent error={requestError}>
+          <DeleteError
+            handleClick={handleDeleteError}
+          />
+        </ErrorComponent>
+      )}
     </div>
   )
 }
