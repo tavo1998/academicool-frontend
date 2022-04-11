0599b763758985807165125c44cea458a5522eee
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-10 19:00:19 -0500

    display username when role is attendant

--- components/side_menu/SideHeader.jsx
+++ components/side_menu/SideHeader.jsx
@@ -6,6 +6,7 @@ import useStore from "../../store";
 const SideHeader = ({ name, description, handleOpen }) => {
   const user = useUser();
   const setIsSideBarOpen = useStore(state => state.setIsSideBarOpen)
+  const student = useStore(state => state.studentSelected)
 
   return (
     <div>
@@ -14,7 +15,12 @@ const SideHeader = ({ name, description, handleOpen }) => {
       </button>
       <div className="flex flex-col items-center">
         <div className="rounded-full h-24 lg:h-20 w-24 lg:w-20 bg-white"/>
-        <h1 className="mt-2 text-white font-semibold lg:text-xl">{user.first_name} {user.last_name}</h1>
+        <h1 className="mt-2 text-white font-semibold lg:text-xl">
+          { student ? 
+            `${student.first_name} ${student.last_name}` :
+            `${user.first_name} ${user.last_name}`
+          }
+        </h1>
         <h1 className="text-white text-sm font-light text-opacity-80 lg:text-base">{getUserRoleName(user.role)}</h1>
       </div>
     </div>
