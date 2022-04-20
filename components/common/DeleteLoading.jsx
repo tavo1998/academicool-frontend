99f04064164bf1257a2a8d99aecb8fcaf68baf70
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2022-04-20 14:39:54 -0500

    fixed popup scroll bug

--- components/common/DeleteLoading.jsx
+++ components/common/DeleteLoading.jsx
@@ -1,6 +1,6 @@
 const DeleteLoading = ({ itemType }) => {
   return (
-    <div className='flex justify-center items-center absolute top-0 left-0 w-full h-screen bg-black bg-opacity-20'>
+    <div className='flex justify-center items-center fixed lg:absolute top-0 left-0 w-full h-screen bg-black bg-opacity-20'>
       <div className='flex flex-col items-center bg-white p-4 rounded-md'>
         <h1 className='text-customGrey font-semibold'>Eliminando {itemType},</h1>
         <h1 className='text-customGrey font-semibold'>espera un momento...</h1>
